27c71e0aa94ca84979f257cdfc311d04f25ed744
Māris Nartišs <mn@sungis.lv>
2017-05-24 22:20:28 +0300

    Smuks redirekts uz citātiem, ja tas nav norādīts

--- djlodesmuiza/urls.py
+++ djlodesmuiza/urls.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^lapas/', include('django.contrib.flatpages.urls')),
     url(r'^qdb/', include('qdb.urls')),
+    url(r'^$', RedirectView.as_view(url='/quote/qdb/', permanent=False), name='index'),
 ]
